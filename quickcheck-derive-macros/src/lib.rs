34f53abfe4b66b2a215ce31219167da3ad080247
Nathan <nathan.titirangi@gmail.com>
2025-06-10 01:49:12 +1200

    Expand shrink to mostly sensible code

--- quickcheck-derive-macros/src/lib.rs
+++ quickcheck-derive-macros/src/lib.rs
@@ -1,11 +1,11 @@
 use proc_macro::{self, TokenStream};
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(QuickCheck)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, data, .. } = parse_macro_input!(input);
-    let arbitray_impl = match data {
+    let (shrink_impl, arbitray_impl) = match data {
         syn::Data::Struct(data_struct) => match data_struct.fields {
             syn::Fields::Named(fields_named) => {
                 let field_arbitrary_generators = fields_named
@@ -15,15 +15,57 @@ pub fn derive(input: TokenStream) -> TokenStream {
                         let identifier = &field.ident;
                         let ty = &field.ty;
                         quote! {
-                            #identifier: <#ty as ::quickcheck::Arbitrary>::arbitrary(g)
+                            #identifier: <Self as ::quickcheck::Arbitrary>::arbitrary(g)
                         }
                     })
                     .collect::<Vec<_>>();
-                quote! {
-                    Self {
-                        #(#field_arbitrary_generators),*
+                let self_copies = fields_named
+                    .named
+                    .iter()
+                    .map(|field| {
+                        let ident = field
+                            .ident
+                            .clone()
+                            .expect("Named identifier must have an identifier");
+                        let ty = &field.ty;
+                        let unique_self = format_ident!("self_{}", ident);
+                        quote! {
+                            let #unique_self = <Self as ::std::clone::Clone>::clone(&self);
+                        }
+                    })
+                    .collect::<Vec<_>>();
+
+                let cloning_iterator_madness = fields_named.named.iter().map(|field| {
+                    let ident = field
+                        .ident
+                        .clone()
+                        .unwrap();
+                    let other_idents = fields_named
+                        .named
+                        .iter()
+                        .map(|field| field.ident.clone().unwrap())
+                        .filter(|e| e != &ident)
+                        .map(|field_ident| {
+                            let unique_self = format_ident!("self_{}", ident);
+                            quote! {#field_ident: ::core::clone::Clone::clone(&#unique_self.#field_ident)}})
+                        .collect::<Vec<_>>();
+                    let ty = &field.ty;
+                    quote! {
+                        ::std::iter::Iterator::map(&<#ty as ::quickcheck::Arbitrary>::shrink(&self.#ident),
+                            move |e| Self {#ident: e, #(#other_idents),*})
                     }
-                }
+                }).rev().reduce(|a, b| quote! {::std::iter::Iterator::chain(#a, #b)}).unwrap_or(quote!{});
+                (
+                    quote! {
+                        #(#self_copies)*
+                        ::std::boxed::Box::new(#cloning_iterator_madness)
+                    },
+                    quote! {
+                        Self {
+                            #(#field_arbitrary_generators),*
+                        }
+                    },
+                )
             }
             syn::Fields::Unnamed(fields_unnamed) => {
                 let field_arbitrary_generators = fields_unnamed
@@ -36,11 +78,16 @@ pub fn derive(input: TokenStream) -> TokenStream {
                         }
                     })
                     .collect::<Vec<_>>();
-                quote! {
-                    Self(#(#field_arbitrary_generators),*)
-                }
+                (
+                    quote! {
+                        ::quickcheck::empty_shrinker()
+                    },
+                    quote! {
+                        Self(#(#field_arbitrary_generators),*)
+                    },
+                )
             }
-            syn::Fields::Unit => quote! {Self},
+            syn::Fields::Unit => (quote! {::quickcheck::empty_shrinker()}, quote! {Self}),
         },
         syn::Data::Enum(data_enum) => {
             let num_variants = data_enum.variants.len();
@@ -76,17 +123,22 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     quote! {#index => #constructor}
                 })
                 .collect::<Vec<_>>();
-            quote! {
-                match <::core::primitive::usize as ::quickcheck::Arbitrary>::arbitrary(g) % #num_variants {
-                    #(#initialisers),*,
-                    _ => ::core::unreachable!()
-                }
-            }
+            (
+                quote! {
+                    ::quickcheck::empty_shrinker()
+                },
+                quote! {
+                    match <::core::primitive::usize as ::quickcheck::Arbitrary>::arbitrary(g) % #num_variants {
+                        #(#initialisers),*,
+                        _ => ::core::unreachable!()
+                    }
+                },
+            )
         }
-        syn::Data::Union(_) => {
+        syn::Data::Union(_) => (quote! {::quickcheck::empty_shrinker()}, {
             syn::Error::new_spanned(&ident, "Cannot derive QuickCheck for a union yet")
                 .to_compile_error()
-        }
+        }),
     };
     let output = quote! {
         impl ::quickcheck::Arbitrary for #ident
@@ -94,6 +146,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
             #ident: ::core::clone::Clone {
             fn arbitrary(g: &mut ::quickcheck::Gen) -> Self {
                 #arbitray_impl
+            }
+
+            fn shrink(&self) -> Box<dyn Iterator<Item = Self>> {
+                #shrink_impl
             }
         }
     };
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-use quickcheck::Gen;
+use std::clone;
+
+use quickcheck::{Arbitrary, Gen};
 use quickcheck_derive_macros::QuickCheck;
 
-#[derive(Clone, QuickCheck)]
-struct Thing {
-    x: i32,
-}
-
-#[derive(Clone, QuickCheck)]
-struct Vector3 {
-    x: f32,
-    y: f32,
-    z: f32,
+fn shrink(v3: Vector3) -> Box<dyn Iterator<Item = Vector3>> {
+    let v3_x = v3.clone();
+    let v3_y = v3.clone();
+    let v3_z = v3.clone();
+    Box::new(
+        v3.x.shrink()
+            .map(move |e| Vector3 {
+                x: e,
+                y: v3_x.y.clone(),
+                z: v3_x.z.clone(),
+            })
+            .chain(
+                v3.y.shrink()
+                    .map(move |e| Vector3 {
+                        x: v3_y.x.clone(),
+                        y: e,
+                        z: v3_y.z.clone(),
+                    })
+                    .chain(v3.z.shrink().map(move |e| Vector3 {
+                        x: v3_z.x.clone(),
+                        y: v3_z.y.clone(),
+                        z: e,
+                    })),
+            ),
+    )
 }
 
 #[derive(Clone, QuickCheck)]
@@ -30,9 +47,17 @@ enum Something {
 }
 
 fn main() {
-    let x: ::core::primitive::u64 = 3;
-
-    let mut g2 = Gen::new(10);
-    let g = &mut g2;
     println!("Hello, world!");
+}
+
+#[derive(Clone, QuickCheck)]
+struct Vector3 {
+    x: String,
+    y: String,
+    z: String,
+}
+
+#[derive(Clone, QuickCheck)]
+struct Thing {
+    x: i32,
 }
